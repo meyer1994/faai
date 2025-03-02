e579c06aaafd8c0a7a1c478f84ded6cd26d80447
Joao Meyer <1994meyer@gmail.com>
2025-03-02 18:05:05 -0300

    Updating base

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [tailwindcss()],
 });
